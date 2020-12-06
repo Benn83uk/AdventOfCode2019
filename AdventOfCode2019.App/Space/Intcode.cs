6274d6afce969e783cc4364373566ee1e5758548
Ben Noble <ben.noble@thoughtworks.com>
2020-12-06 21:09:54 +0000

    [DayTwoTaskTwo] BN - Finding op params for a given answer

--- AdventOfCode2019.App/Space/Intcode.cs
+++ AdventOfCode2019.App/Space/Intcode.cs
@@ -1,10 +1,12 @@
-using System.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2019.App.Space
 {
     public class Intcode
     {
-        private int[] _ops;
+        private readonly IReadOnlyList<int> _ops;
 
         public Intcode(params int[] ops)
         {
@@ -13,52 +15,70 @@ namespace AdventOfCode2019.App.Space
 
         public int Run()
         {
-            for (var pointer = 0; pointer < _ops.Length; pointer += 4)
-            {
-                if (!Run(pointer)) break;
-            }
-            return _ops[0];
+            return Run(_ops[1], _ops[2]);
+            var opsCopy = _ops.Select(i => i).ToArray();
+
         }
 
         public int Run(int param1, int param2)
         {
-            _ops[1] = param1;
-            _ops[2] = param2;
-            return Run();
+            var opsCopy = _ops.Select(i => i).ToArray();
+            opsCopy[1] = param1;
+            opsCopy[2] = param2;
+            for (var pointer = 0; pointer < _ops.Count; pointer += 4)
+            {
+                if (!Run(opsCopy, pointer)) break;
+            }
+            return opsCopy[0];
         }
 
-        private bool Run(int pointer)
+        private bool Run(int[] ops, int pointer)
         {
-            var op = _ops[pointer];
+            var op = ops[pointer];
             switch (op)
             {
                 case 99:
                     return false;
                 case 1:
-                    Add(pointer);
+                    Add(ops, pointer);
                     break;
                 case 2:
-                    Multiply(pointer);
+                    Multiply(ops, pointer);
                     break;
             }
 
             return true;
         }
 
-        private void Add(int pointer)
+        private void Add(int[] ops, int pointer)
         {
-            var input1 = _ops[_ops[pointer + 1]];
-            var input2 = _ops[_ops[pointer + 2]];
-            var storePosition = _ops[pointer + 3];
-            _ops[storePosition] = input1 + input2;
+            var input1 = ops[ops[pointer + 1]];
+            var input2 = ops[ops[pointer + 2]];
+            var storePosition = ops[pointer + 3];
+            ops[storePosition] = input1 + input2;
         }
         
-        private void Multiply(int pointer)
+        private void Multiply(int[] ops, int pointer)
         {
-            var input1 = _ops[_ops[pointer + 1]];
-            var input2 = _ops[_ops[pointer + 2]];
-            var storePosition = _ops[pointer + 3];
-            _ops[storePosition] = input1 * input2;
+            var input1 = ops[ops[pointer + 1]];
+            var input2 = ops[ops[pointer + 2]];
+            var storePosition = ops[pointer + 3];
+            ops[storePosition] = input1 * input2;
+        }
+
+        public int[] RunFor(int expected)
+        {
+            var limit = Math.Min(_ops.Count, 100);
+            for (var noun = 0; noun < limit; noun++)
+            {
+                for (var verb = 0; verb < limit; verb++)
+                {
+                    var actual = Run(noun, verb);
+                    if (actual == expected) return new int[] {noun, verb};
+                }
+            }
+
+            return Array.Empty<int>();
         }
     }
 }
--- AdventOfCode2019.Test/Space/IntcodeTest.cs
+++ AdventOfCode2019.Test/Space/IntcodeTest.cs
@@ -7,13 +7,6 @@ namespace AdventOfCode2019.Test.Space
     public class IntcodeTest
     {
         [Test]
-        public void ImmediateTerminationReturns99()
-        {
-            var intcode = new Intcode(99);
-            Assert.That(intcode.Run(), Is.EqualTo(99));
-        }
-        
-        [Test]
         public void AddAndStoreAtZero()
         {
             var intcode = new Intcode(1, 5, 6, 0, 99, 8, 9);
@@ -53,6 +46,13 @@ namespace AdventOfCode2019.Test.Space
         {
             var intcode = new Intcode(1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,1,10,19,23,2,9,23,27,1,6,27,31,1,10,31,35,1,35,10,39,1,9,39,43,1,6,43,47,1,10,47,51,1,6,51,55,2,13,55,59,1,6,59,63,1,10,63,67,2,67,9,71,1,71,5,75,1,13,75,79,2,79,13,83,1,83,9,87,2,10,87,91,2,91,6,95,2,13,95,99,1,10,99,103,2,9,103,107,1,107,5,111,2,9,111,115,1,5,115,119,1,9,119,123,2,123,6,127,1,5,127,131,1,10,131,135,1,135,6,139,1,139,5,143,1,143,9,147,1,5,147,151,1,151,13,155,1,5,155,159,1,2,159,163,1,163,6,0,99,2,0,14,0);
             Assert.That(intcode.Run(12, 2), Is.EqualTo(11590668));
+        }
+
+        [Test]
+        public void FindAnswerFor()
+        {
+            var intcode = new Intcode(1, 0, 0, 0, 99, 8, 9);
+            Assert.That(intcode.RunFor(17), Is.EquivalentTo(new int[]{5,6}));
         }
     }
 }
